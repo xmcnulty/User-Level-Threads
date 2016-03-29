50b7c43c2cf0437cbd68c9dd59de1a74fa43c283
xmcnulty <x.mcnulty@gmail.com>
2016-03-29 19:32:22 -0400

    Created sigaction and sigaltstack in main.

--- User Level Threads/ulthreads.c
+++ User Level Threads/ulthreads.c
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <setjmp.h>
+#include <signal.h>
 
 // Execution state (machine context) data structure
 typedef struct xstate_st {
@@ -25,7 +26,10 @@ void xstate_switch(xstate_t *xstate);
    main_arg is a pointer to a possible argument to the main function.
    stack is the memory buffer set aside for the thread stack.
    stack_size is the size in bytes of the stack */
-void xstate_create(xstate_t *xstate, void (*main) (void *), void *main_arg, void *stack, size_t stack_size);
+void xstate_create(xstate_t *xstate, void (*main) (void *), void *main_arg, void *stack, size_t stack_size) {
+    // create the xstate
+    xstate = (xstate_t*) calloc(1, sizeof(xstate_t));
+}
 
 /* Private signal handler function that will be called with a signal stack.
    The execution state will be saved before returning from the handler. 
@@ -36,4 +40,40 @@ void xstate_create(xstate_t *xstate, void (*main) (void *), void *main_arg, void
 void xstate_capture_stack(int sig);
 
 /* Start the newly create thread by making a call to the main function of the thread. */
-void xstate_boot(void);
+/* this test function squares a number and prints the result. */
+void xstate_boot(int n) {
+    int square = n*n;
+    
+    printf("%d squared is: %d\n", n, square);
+    fflush(stdout);
+}
+
+// global instance of xstate_t objects. Used for testing.
+xstate_t *state1;
+int num = 3;
+
+int main(int argc, char ** argv) {
+    /* create the stack for state1 */
+    void *state1_stack = (void*) malloc(MINSIGSTKSZ);
+    
+    /* Specifies state1_stack as the alternate stack on which the SIGUSR1
+        signal is to be processed. */
+    stack_t state1_ss = {
+        .ss_size = MINSIGSTKSZ,
+        .ss_sp = state1_stack
+    };
+    
+    /* Specifies xstate_capture_stack as the alternal handler function to be called when
+        a signal is processed. */
+    struct sigaction state1_sa = {
+        .sa_handler = xstate_capture_stack,
+        .sa_flags = SA_ONSTACK
+    };
+    
+    /* set state1_stack as the alternate stack used for signal processing. */
+    sigaltstack(&state1_ss, 0);
+    
+    /* set the sigaction that calls xstate_capture_stack as the function to
+       process a signal. */
+    sigaction(SIGUSR1, &state1_sa, 0);
+}
