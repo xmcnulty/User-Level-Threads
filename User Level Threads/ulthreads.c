d8cf9e68b357de94e5a2941f7d5a71d53070122d
xmcnulty <x.mcnulty@gmail.com>
2016-03-31 15:14:39 -0400

    Have grasped a better understanding of the project specs, committing changes made to better satisfy project specifications.

--- User Level Threads/ulthreads.c
+++ User Level Threads/ulthreads.c
@@ -14,12 +14,28 @@
 #include <unistd.h>
 
 // Execution state (machine context) data structure
-typedef struct xstate_st {
-    jmp_buf jb;
+typedef struct xstate_t {
+    jmp_buf jb; // jmp_buf used for context switching
+    
+    void (*func) (void*); // function called by xstate
+    void *args; // arguments for func
+    
 } xstate_t;
 
+/* Specifies state1_stack as the alternate stack on which the SIGUSR1
+ signal is to be processed. */
+stack_t ss;
+
+// global instance of xstate_t objects. Used for testing.
+xstate_t *state1;
+
+// global starting address for state1 stack
+void *state1_stack;
+
+int num = 3; // global parameter for test function
+
 /* Execution state control functions: save, restore, switch. */
-#define xstate_save(xstate) xstate->setjmp_ret = setjmp(xstate->jb)
+#define xstate_save(xstate) setjmp(xstate->jb)
 #define xstate_restore(xstate) longjmp(xstate->jb, xstate->setjmp_ret)
 #define xstate_switch(old_xstate, new_xstate) \
     if (setjmp(old_xstate->jb) == 0) \
@@ -30,8 +46,19 @@ typedef struct xstate_st {
    stack is the memory buffer set aside for the thread stack.
    stack_size is the size in bytes of the stack */
 void xstate_create(xstate_t *xstate, void (*main) (void *), void *main_arg, void *stack, size_t stack_size) {
-    // create the xstate
-    xstate = (xstate_t*) calloc(1, sizeof(xstate_t));
+    /* sets values of ss to create the stack when the user signal is handled */
+    ss.ss_size = stack_size;
+    ss.ss_sp = stack;
+    ss.ss_flags = SA_ONSTACK;
+    
+    /* allocate the xstate and set its contents */
+    if ((xstate = (xstate_t*) calloc(1, sizeof(xstate_t))) == 0) {
+        perror("allocating xstate.\n");
+        exit(-1);
+    }
+    
+    xstate->func = main;
+    xstate->args = main_arg;
 }
 
 /* Private signal handler function that will be called with a signal stack.
@@ -41,6 +68,8 @@ void xstate_create(xstate_t *xstate, void (*main) (void *), void *main_arg, void
    with the alternative stack in place and the function xstate_boot will
    be called */
 void xstate_capture_stack(int sig) {
+    xstate_save(state1);
+    
     printf("in xstate_capture_stack: %d\n", sig);
     fflush(stdout);
     
@@ -50,9 +79,10 @@ void xstate_capture_stack(int sig) {
 /* Start the newly create thread by making a call to the main function of the thread. */
 /* this test function squares a number and prints the result. */
 void xstate_boot(void) {
-    
+    ((void (*) (int))state1->func) (*((int *) state1->args));
 }
 
+/* test function */
 void test (int n) {
     int i;
     
@@ -62,13 +92,6 @@ void test (int n) {
     }
 }
 
-
-/* Specifies state1_stack as the alternate stack on which the SIGUSR1
- signal is to be processed. */
-stack_t ss = {
-    .ss_size = MINSIGSTKSZ,
-};
-
 /* Specifies xstate_capture_stack as the alternal handler function to be called when
  a signal is processed. */
 struct sigaction sa = {
@@ -76,35 +99,22 @@ struct sigaction sa = {
     .sa_flags = SA_ONSTACK,
 };
 
-// global instance of xstate_t objects. Used for testing.
-xstate_t *state1;
-
-// global starting address for state1 stack
-void *state1_stack;
-
-int num = 3; // global parameter for test function
-
 /* Main function, used for testing. */
 int main(int argc, char ** argv) {
-    /* create the stack for state1 */
-    state1_stack = (void*) malloc(MINSIGSTKSZ);
+    /* allocate space for the state1 stack */
+    state1_stack = (void*) calloc(MINSIGSTKSZ, sizeof(void));
     
-    // set ss to the newly allocated state1_stack
-    ss.ss_sp = state1_stack;
+    /* create the xstate state1 */
+    xstate_create(state1, (void (*) (void *)) &test, (void*) &num, state1_stack, MINSIGSTKSZ);
     
-    /* set up the alternate stack and signal action */
-    sigaltstack(&ss, 0);
-    //sigemptyset(&sa.sa_mask);
-    sigaction(SIGUSR1, &sa, 0);
+    sigfillset(&sa.sa_mask);
+    sigaddset(&sa.sa_mask, SIGUSR1);
     
-    sigset_t mask, oldmask; // mask for blocing SIGUSR1 and the oldset before that
+    sigset_t oldset; // old signal set, (without SIGUSR1)
     
-    sigemptyset(&mask);
-    sigaddset(&mask, SIGUSR1);
-    
-    sigprocmask(SIG_BLOCK, &mask, &oldmask);
+    sigprocmask(SIG_BLOCK, &sa.sa_mask, &oldset);
     
     kill(getpid(), SIGUSR1);
     
-    sigprocmask(SIG_UNBLOCK, &mask, &oldmask);
+    sigprocmask(SIG_UNBLOCK, &sa.sa_mask, 0);
 }
